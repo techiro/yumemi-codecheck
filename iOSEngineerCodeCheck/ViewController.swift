311abe4543d5f43c246f202fa0918f6bd563256c
techiro <techiromen0@gmail.com>
2021-10-26 11:22:42 +0900

    refactor: comment

--- iOSEngineerCodeCheck/ViewController.swift
+++ iOSEngineerCodeCheck/ViewController.swift
@@ -17,17 +17,16 @@ class ViewController: UITableViewController, UISearchBarDelegate {
     var task: URLSessionTask?
     var word: String!
     var url: String!
-    var idx: Int!
+    var index: Int!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         searchBar.text = "GitHubのリポジトリを検索できるよー"
         searchBar.delegate = self
     }
 
     func searchBarShouldBeginEditing(_ searchBar: UISearchBar) -> Bool {
-        // ↓こうすれば初期のテキストを消せる
+        // TODO: 検索バーの初期値はなし、プレースホルダーで示してあげる。
         searchBar.text = ""
         return true
     }
@@ -66,7 +65,7 @@ class ViewController: UITableViewController, UISearchBarDelegate {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
 
         if segue.identifier == "Detail"{
-            let dtl = segue.destination as! ViewController2
+            let dtl = segue.destination as! DetailViewController
             dtl.vc1 = self
         }
 
@@ -87,9 +86,8 @@ class ViewController: UITableViewController, UISearchBarDelegate {
 
     }
 
+    // MARK: 画面遷移
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        // 画面遷移時に呼ばれる
-        idx = indexPath.row
         performSegue(withIdentifier: "Detail", sender: self)
 
     }
