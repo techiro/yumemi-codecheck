1d6aeb10c90dfb2017b46828c872eaa1643477dc
techiro <techiromen0@gmail.com>
2021-10-26 14:48:44 +0900

    fix: vertical name.

--- iOSEngineerCodeCheck/DetailViewController.swift
+++ iOSEngineerCodeCheck/DetailViewController.swift
@@ -21,29 +21,29 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var forksLabel: UILabel!
     @IBOutlet weak var issuesLabel: UILabel!
 
-    var vc1: ViewController!
+    var searchVC: SearchViewController!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let repo = vc1.repo[vc1.index]
+        let repository = searchVC.repositorys[searchVC.index]
 
-        languageLabel.text = "Written in \(repo["language"] as? String ?? "")"
-        starsLabel.text = "\(repo["stargazers_count"] as? Int ?? 0) stars"
-        watchersLabel.text = "\(repo["wachers_count"] as? Int ?? 0) watchers"
-        forksLabel.text = "\(repo["forks_count"] as? Int ?? 0) forks"
-        issuesLabel.text = "\(repo["open_issues_count"] as? Int ?? 0) open issues"
+        languageLabel.text = "Written in \(repository["language"] as? String ?? "")"
+        starsLabel.text = "\(repository["stargazers_count"] as? Int ?? 0) stars"
+        watchersLabel.text = "\(repository["wachers_count"] as? Int ?? 0) watchers"
+        forksLabel.text = "\(repository["forks_count"] as? Int ?? 0) forks"
+        issuesLabel.text = "\(repository["open_issues_count"] as? Int ?? 0) open issues"
         getImage()
 
     }
 
     func getImage() {
 
-        let repo = vc1.repo[vc1.index]
+        let repository  = searchVC.repositorys[searchVC.index]
 
-        titleLabel.text = repo["full_name"] as? String
+        titleLabel.text = repository["full_name"] as? String
 
-        if let owner = repo["owner"] as? [String: Any] {
+        if let owner = repository["owner"] as? [String: Any] {
             if let imgURL = owner["avatar_url"] as? String {
                 URLSession.shared.dataTask(with: URL(string: imgURL)!) { (data, res, err) in
                     let img = UIImage(data: data!)!
