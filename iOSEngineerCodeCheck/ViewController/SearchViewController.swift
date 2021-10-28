620beca51b7b83ad6aae5f5a3d7a8ccc111267ba
techiro <techiromen0@gmail.com>
2021-10-28 16:41:43 +0900

    fix:the way urlsesstion is called

--- iOSEngineerCodeCheck/ViewController/SearchViewController.swift
+++ iOSEngineerCodeCheck/ViewController/SearchViewController.swift
@@ -7,12 +7,11 @@
 //
 
 import UIKit
-
 class SearchViewController: UITableViewController, UISearchBarDelegate {
 
     @IBOutlet weak var searchBar: UISearchBar!
 
-    var repositorys: [[String: Any]] = []
+    var repositories: [Repository] = []
 
     var task: URLSessionTask?
     var word: String = ""
@@ -35,31 +34,18 @@ class SearchViewController: UITableViewController, UISearchBarDelegate {
 
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
 
-        word = searchBar.text!
-
-        if !word.isEmpty {
-            url = "https://api.github.com/search/repositories?q=\(word)"
-            // TODO: 文字列デコード
-
-            task = URLSession.shared.dataTask(with: URL(string: url)!) { (data, res, err) in
-                do {
-                    if let obj = try JSONSerialization.jsonObject(with: data!) as? [String: Any] {
-                        if let items = obj["items"] as? [[String: Any]] {
-                            self.repositorys = items
-                            DispatchQueue.main.async {
-                                self.tableView.reloadData()
-                            }
-                        }
-                    }
-                } catch {
-                    print(error)
+        guard let word = searchBar.text else { return }
+        GitHubAPI.fetchRepositories(word: word) { [weak self] result in
+            switch result {
+            case .success(let repositories):
+                self?.repositories = repositories.items
+                DispatchQueue.main.async {
+                    self?.tableView.reloadData()
                 }
-
+            case .failure(let error):
+                print(error.localizedDescription)
             }
-            // これ呼ばなきゃリストが更新されません
-            task?.resume()
         }
-
     }
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -68,19 +54,18 @@ class SearchViewController: UITableViewController, UISearchBarDelegate {
             let detailVC = segue.destination as! DetailViewController
             detailVC.searchVC = self
         }
-
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return repositorys.count
+        return repositories.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 
         let cell = UITableViewCell()
-        let rp = repositorys[indexPath.row]
-        cell.textLabel?.text = rp["full_name"] as? String ?? ""
-        cell.detailTextLabel?.text = rp["language"] as? String ?? ""
+        let rp = repositories[indexPath.row]
+        cell.textLabel?.text = rp.fullName
+        cell.detailTextLabel?.text = rp.language
         cell.tag = indexPath.row
         return cell
 
