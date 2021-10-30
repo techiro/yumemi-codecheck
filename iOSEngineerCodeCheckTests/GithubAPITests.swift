9c71bc003f0edd6186252b2347a1de7c26e694e0
techiro <techiromen0@gmail.com>
2021-10-30 09:58:39 +0900

    add: GithubAPITests

---
+++ iOSEngineerCodeCheckTests/GithubAPITests.swift
@@ -0,0 +1,47 @@
+//
+//  GithubAPITests.swift
+//  iOSEngineerCodeCheckTests
+//
+//  Created by TanakaHirokazu on 2021/10/29.
+//
+import Foundation
+@testable import iOSEngineerCodeCheck
+import XCTest
+
+class GitHubAPITests: XCTestCase {
+
+    var githubAPI: MockGitHubAPI!
+
+    override func setUp() {
+        super.setUp()
+        githubAPI = MockGitHubAPI()
+    }
+    func test_fetchItemsIsEqualToMockItems() {
+        githubAPI.fetchRepositories(word: "") {  result in
+            switch result {
+            case .success(let items):            XCTAssertEqual(items, SearchRepositoriesResponse.mock())
+            case .failure(_):
+                fatalError()
+            }
+        }
+
+    }
+
+    func test_whenFailStatuscodeError() {
+        githubAPI.isSuccess = false
+        githubAPI.fetchRepositories(word: "") { result in
+            switch result {
+            case .success(_): break
+            case .failure(let error):
+                switch error {
+                case .statusCode(let message):
+                    XCTAssertEqual(message, "mock statusError")
+
+                default:
+                    fatalError()
+                }
+            }
+        }
+    }
+    
+}
