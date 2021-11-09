9753f77457d7fc23765dbbf9560811e59fd757f2
techiro <techiromen0@gmail.com>
2021-11-09 16:28:38 +0900

    add: RepositoryCell

---
+++ iOSEngineerCodeCheck/SearchRepository/RepositoryCell.swift
@@ -0,0 +1,42 @@
+//
+//  RepositoryCell.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by TanakaHirokazu on 2021/11/07.
+//
+
+import UIKit
+
+final class RepositoryCell: UITableViewCell {
+    @IBOutlet private weak var iconImageView: UIImageView!
+    @IBOutlet private weak var nameLabel: UILabel!
+    @IBOutlet weak var languageLabel: UILabel!
+    func setup(repository: Repository) {
+        setupImage(repository: repository)
+        DispatchQueue.main.async {
+            self.nameLabel.text = repository.fullName
+            self.languageLabel.text = repository.language
+        }
+    }
+
+    func setupImage(repository: Repository) {
+        ImageAPI().getImage(from: repository.owner?.avatarUrl) { [weak self] result in
+            switch result {
+            case .success(let data):
+                let img = UIImage(data: data)
+                DispatchQueue.main.async { [weak self] in
+                    self?.iconImageView?.image = img
+
+                }
+            case .failure(let error):
+                switch error {
+                case .dataError(let title):
+                    print(title)
+                case .urlError(let title):
+                    print(title)
+
+                }
+            }
+        }
+    }
+}
