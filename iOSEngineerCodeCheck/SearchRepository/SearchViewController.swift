ecd308654295fbbddab39272b1f085605ab4367a
techiro <techiromen0@gmail.com>
2021-11-09 16:29:44 +0900

    fix: SearchViewController

--- iOSEngineerCodeCheck/SearchRepository/SearchViewController.swift
+++ iOSEngineerCodeCheck/SearchRepository/SearchViewController.swift
@@ -7,95 +7,86 @@
 //
 
 import UIKit
-class SearchViewController: UITableViewController {
+
+class SearchViewController: UIViewController {
 
     @IBOutlet weak private var searchBar: UISearchBar!
-    private var alert = AlertViewController()
-    private var repositories: [Repository] = []
-    // swiftlint:disable implicitly_unwrapped_optional
-    private var selectedRepository: Repository!
-    private var apiRequest: GitHubAPIProtocol!
+    @IBOutlet weak private var tableView: UITableView!
 
+    private var presenter: SearchRepositoryInput!
     override func viewDidLoad() {
         super.viewDidLoad()
-        searchBar.delegate = self
-        alert.delegate = self
-        self.tableView.register(RepositoryCell.self, forCellReuseIdentifier: "RepositoryCell")
-        self.apiRequest = GitHubAPI()
+        setup()
     }
 
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "Detail" {
-            guard let detailVC = segue.destination as? DetailViewController else { return }
-            detailVC.repository = selectedRepository
-        }
+    // Dependency Injection 依存関係の注入
+    func inject(presenter: SearchRepositoryInput) {
+        self.presenter = presenter
+    }
+
+    //    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    //        if segue.identifier == "Detail" {
+    //            guard let detailVC = segue.destination as? DetailViewController else { return }
+    //            detailVC.repository = selectedRepository
+    //        }
+    //    }
+
+    private func setup() {
+        // MARK: StoryboardのOutlet
+        searchBar.delegate = self
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.estimatedRowHeight = 64
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.register(UINib(nibName: "RepositoryCell", bundle: nil), forCellReuseIdentifier: "RepositoryCell")
     }
 
 }
 
-// MARK: 検索機能
+extension SearchViewController: SearchRepositoryPresenterOutput {
+    func presentDetail(at: IndexPath) {
+
+    }
+
+    func updateRepositories(_ repositories: [Repository]) {
+        tableView.reloadData()
+    }
+}
+
+// TableViewの操作
+extension SearchViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        presenter.selectRow(at: indexPath)
+        // ここで色々なロジックは含まれない
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+}
+
+// MARK: TableView
+extension SearchViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RepositoryCell") as! RepositoryCell
+        let repository = presenter.repository(index: indexPath)
+        cell.setup(repository: repository)
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return presenter.numberOfRepositories
+    }
+
+}
+
+// MARK: 検索バー
 extension SearchViewController: UISearchBarDelegate {
     func searchBarShouldBeginEditing(_ searchBar: UISearchBar) -> Bool {
         return true
     }
 
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-
-        guard let word = searchBar.text else { return }
-
-        apiRequest.fetchRepositories(word: word) { [weak self] result in
-            switch result {
-            case .success(let repositories):
-                self?.repositories = repositories.items
-                DispatchQueue.main.async {
-                    self?.tableView.reloadData()
-                }
-            case .failure(let error):
-                switch error {
-                case .statusCode(let title):
-                    self?.alert.setAlert(title: title, message: "もう一度検索してください")
-
-                case .parse(let title):
-                    self?.alert.setAlert(title: title, message: "もう一度検索してください")
-                }
-            }
-            DispatchQueue.main.async {
-                self?.view.endEditing(true)
-            }
-
-        }
-    }
-}
-
-// MARK: テーブルView
-extension SearchViewController {
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return repositories.count
-    }
-
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-
-        let repository = repositories[indexPath.row]
-        let cell = tableView.dequeueReusableCell(withIdentifier: "RepositoryCell", for: indexPath)
-        cell.textLabel?.text = repository.fullName
-        cell.accessoryType = .disclosureIndicator
-        cell.detailTextLabel?.text = repository.language
-        return cell
-    }
-
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        selectedRepository = repositories[indexPath.row]
-        performSegue(withIdentifier: "Detail", sender: self)
-        self.view.endEditing(true)
-    }
-}
-
-// MARK: Alertを表示
-extension SearchViewController: UIAlertDelegate {
-
-    func displayAlert(alert: UIAlertController) {
-        DispatchQueue.main.async { [weak self] in
-            self?.present(alert, animated: true, completion: nil)
-        }
+        presenter.tap(searchBar)
     }
 }
