8442b2f95479abb92f3b39f154a155854f39206f
techiro <techiromen0@gmail.com>
2021-11-10 08:30:19 +0900

    fix: 画像取得API

--- iOSEngineerCodeCheck/API/ImageAPI.swift
+++ iOSEngineerCodeCheck/API/ImageAPI.swift
@@ -8,24 +8,20 @@
 import Foundation
 
 final class ImageAPI {
-
+    
     func getImage(from url: String?, completion: @escaping (Result<Data, ImageError>) -> Void) {
-
-        if let imgURL = url {
-            // swiftlint:disable force_unwrapping
-            // TODO: fix: force unwrapping
-            URLSession.shared.dataTask(with: URL(string: imgURL)!) { (data, _, _) in
-                if let data = data {
-                    completion(.success(data))
-                } else {
-                    completion(.failure(.dataError(message: "画像が読み込めません")))
-                }
-            }.resume()
-        } else {
-            completion(.failure(.urlError(message: "urlが正しくありません")))
-        }
+        guard let url = url else { return completion(.failure(.urlError(message: "urlがStringではありません")))}
+        guard let imgURL = URL(string: url) else { return completion(.failure(.urlError(message: "urlが正しくありません"))) }
+        
+        URLSession.shared.dataTask(with: imgURL) { (data, _, _) in
+            if let data = data {
+                completion(.success(data))
+            } else {
+                completion(.failure(.dataError(message: "画像が読み込めません")))
+            }
+        }.resume()
     }
-
+    
     enum ImageError: Error {
         case urlError(message: String)
         case dataError(message: String)
@@ -35,13 +31,13 @@ final class ImageAPI {
 #if DEBUG
 class MockImageAPI {
     var isSuccess: Bool
-
+    
     init(isSuccess: Bool = true) {
         self.isSuccess = isSuccess
     }
-
+    
     func getImage(from url: String?, completion: @escaping (Result<Data, ImageAPI.ImageError>) -> Void) {
-
+        
         if isSuccess {
             completion(.success(Data()))
         } else {
